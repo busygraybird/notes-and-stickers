ef114faa254c864558768110469d95b913303868
Viktoria <tmw.kva@yandex.ru>
2021-12-28 16:03:42 +0700

    Init project

---
+++ rollup.config.js
@@ -0,0 +1,58 @@
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import html from 'rollup-plugin-html';
+import postcss from 'rollup-plugin-postcss';
+import stylelint from 'stylelint';
+import image from '@rollup/plugin-image';
+import typescript from "rollup-plugin-typescript2";
+import {eslint} from "rollup-plugin-eslint";
+
+export default {
+  input: 'src/index.tsx',
+  output: {
+    file: 'build/bundle.js',
+    format: 'iife',
+    sourcemap: true,
+  },
+  plugins: [
+    html({
+      include: '**/*.html',
+    }),
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+    image(),
+    resolve({
+      jsnext: true,
+      module: true,
+      browser: true,
+      extensions: ['.js', '.ts', '.tsx'],
+    }),
+    eslint(),
+    commonjs({
+      exclude: 'src/**',
+    }),
+    typescript(),
+    stylelint(),
+    postcss({
+      extract: false,
+      modules: {
+        generateScopedName: '[name]__[local]___[hash:base64:5]',
+        localsConvention: 'camelCaseOnly',
+      },
+      use: ['sass'],
+    }),
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: ['', 'public'],
+      host: 'localhost',
+      port: 3000,
+    }),
+    livereload({ watch: 'build' }),
+  ],
+};
---
+++ src/ui/App/App.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return <div>Notes & Stickers</div>;
+};
+
+export default App;
