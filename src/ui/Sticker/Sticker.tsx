3110f9ae6c98679c6aad306064d3bd25a0e3c18b
Viktoria <tmw.kva@yandex.ru>
2021-12-30 09:40:34 +0700

    Change sticker size

--- src/ui/Sticker/Sticker.tsx
+++ src/ui/Sticker/Sticker.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 
+import styles from './Sticker.module.scss';
+
 interface ISticker {
   src?: string;
 }
 
 const Sticker: FC<ISticker> = ({ src }) => {
   // TODO: add alt to domain
-  return <img src={src} alt="" />;
+  return <img src={src} className={styles.sticker} alt="" />;
 };
 
 export default Sticker;
